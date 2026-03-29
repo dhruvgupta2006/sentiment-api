96ddf97157eee6307e38f53ac6fdcb07c0e7aa95
dhruvgupta2006 <dhruvgupta.bt25ecevlsi@pec.edu.in>
2026-03-29 15:43:07 +0530

    optimize memory in main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from transformers import pipeline
 
-sentiment_model = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base")
+sentiment_model = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base",device=-1,torch_dtype="auto")
 
 
 app = FastAPI()
